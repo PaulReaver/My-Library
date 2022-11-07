f5342c2099c7bc8cd689b4eff02a5feeb474608d
Paul Vallas <35564306+PaulReaver@users.noreply.github.com>
2022-11-07 14:29:26 +0200

    Books now appear on main container

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //Array that holds all book objects
 let myLibrary = [
     new Book("Ninja Techniques", "Sensei", 23, false),
+    new Book("Cooking Styles", "The Chef", 243, true),
+    new Book("Cooking Styles", "The Chef", 243, true),
+    new Book("Cooking Styles", "The Chef", 243, true),
+    new Book("Cooking Styles", "The Chef", 243, true),
+    new Book("Cooking Styles", "The Chef", 243, true),
+    new Book("Cooking Styles", "The Chef", 243, true),
     new Book("Cooking Styles", "The Chef", 243, true)
 ];
 
@@ -14,5 +20,11 @@ function Book(title, author, pages, status) {
 
 //Adds book objects to the library
 function addBookToLibrary() {
-
+    for (let i = 0; i < myLibrary.length; i++) {
+        let currentBook = document.createElement("div")
+        currentBook.classList.add("book-card");
+        document.querySelector(".main-container").appendChild(currentBook);
+    }
 }
+
+addBookToLibrary();
