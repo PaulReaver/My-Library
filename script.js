bf62fe53a6eb860108ebd8f3028289c20b5f1a7d
Paul Vallas <35564306+PaulReaver@users.noreply.github.com>
2022-11-07 15:24:48 +0200

    Status buttons styled and working

--- script.js
+++ script.js
@@ -21,9 +21,30 @@ function Book(title, author, pages, status) {
 //Adds book objects to the library
 function addBookToLibrary() {
     for (let i = 0; i < myLibrary.length; i++) {
+        //Creates book card with a class
         let currentBook = document.createElement("div")
         currentBook.classList.add("book-card");
         document.querySelector(".main-container").appendChild(currentBook);
+
+        //Creates the status button
+        let currentStatus = document.createElement("button");
+        if (myLibrary[i].status == false) {
+            currentStatus.classList.add("status-button-false");
+            currentStatus.textContent = "Not Read";
+        } else {
+            currentStatus.classList.add("status-button-true");
+            currentStatus.textContent = "Read";
+        }
+        currentBook.appendChild(currentStatus);
+        currentStatus.addEventListener("click", () => {
+            if (currentStatus.className == "status-button-false") {
+                currentStatus.className = "status-button-true";
+                currentStatus.textContent = "Read";
+            } else {
+                currentStatus.className = "status-button-false";
+                currentStatus.textContent = "Not Read";
+            }
+        });
     }
 }
 
